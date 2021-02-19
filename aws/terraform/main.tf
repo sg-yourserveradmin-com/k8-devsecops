513fd22768b8b2136fe8cc63108f998d8f0b6e48
sg <sg@yourserveradmin.com>
2021-02-19 12:28:59 +0200

    Add initial TF configuration and variables, along with bootstrap module to create S3 bucket and DynamoDB (to be used as backend)

---
+++ aws/terraform/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_version = ">=0.14.7"
+  #  backend "s3" {
+  #    bucket         = "glasswall-github-actions-terraform-tfstate"
+  #    key            = "terraform.tfstate"
+  #    region         = "eu-west-1"
+  #    dynamodb_table = "terraform"
+  #    encrypt        = true
+  #  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.28.0"
+    }
+  }
+}
+
+# We will use this simple module to bootstrap S3 bucket for keeping TF state, and
+# DynamoDB to provide state locks
+module "prepare-s3-backend" {
+  source               = "./modules/prepare-s3-backend"
+  s3_bucket_name       = "glasswall-github-actions-terraform-tfstate"
+  dynamo_db_table_name = "terraform"
+}
